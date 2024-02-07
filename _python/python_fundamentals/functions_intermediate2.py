b61a88a2a70e109dc0250717b5b1dfc2eba0a2a6
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-07 09:53:14 +0200

    implemented iterate through a list of dictionaries functionality

--- _python/python_fundamentals/functions_intermediate2.py
+++ _python/python_fundamentals/functions_intermediate2.py
@@ -26,12 +26,13 @@ students = [
          {'first_name' : 'Mark', 'last_name' : 'Guillen'},
          {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-iterateDictionary(students) 
-# should output: (it's okay if each key-value pair ends up on 2 separate lines;
-# bonus to get them to appear exactly as below!)
-first_name - Michael, last_name - Jordan
-first_name - John, last_name - Rosales
-first_name - Mark, last_name - Guillen
-first_name - KB, last_name - Tonel
 
+def iterateDictionary(input_list):
+	 for input in input_list:
+	 	f_name = input["first_name"]
+	 	l_name = input["last_name"]
+	 	print("first_name -",f_name,", last_name -",l_name)
+
+	
+iterateDictionary(students)
 
