de024e8a82aab156a6e6efc21d922ee135251f3f
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-07 02:02:33 +0200

    added bonus requirement to functions intermediate 1 assignment

--- _python/python_fundamentals/functions_intermediate1.py
+++ _python/python_fundamentals/functions_intermediate1.py
@@ -1,7 +1,13 @@
 import random
 def randInt(min= 0  , max= 100  ):
-    num = random.random() * max + min
-    return round(num)
+	if min > max:
+		print("please make sure max is greater than min")
+		return False
+	if max < 0:
+		print("please make sure max is a positive number greater than 0")
+		return False
+	num = random.random() * max + min
+	return round(num)
 #should print a random integer between 0 to 100
 print(randInt())
 """random100 = randInt()
@@ -14,4 +20,5 @@ print(random100,"after",count)
 print(randInt(max=50))		# should print a random integer between 0 to 50
 print(randInt(min=50))		#should print a random integer between 50 to 100
 print(randInt(min=50, max=500))	#should print a random integer between 50 and 500
-
+print(randInt(max=50, min = 500)) # should return false
+print(randInt(min = -20, max = -10)) # should return false
