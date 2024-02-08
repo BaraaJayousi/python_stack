0dd3943cd3fa9d191d8a791e3b2040546867dea1
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-08 03:04:39 +0200

    optimised the sorting algorithim so it stops looping if the array is sorted by adding a swapping tracker

--- _python/python_fundamentals/buble_sort.py
+++ _python/python_fundamentals/buble_sort.py
@@ -1,10 +1,16 @@
-test_list = [2,5,1,3,6,8,4,7]
+test_list = [1,2,3,4,5,6,8,7]
 
 def bubble_sort(input_list):
 	for i in range(len(input_list)):
+		#swapped varialbe keeps track if any swapping occured in the second loop. if no swapps occured then the array is sorted hence no need to keep the parent loop
+		swapped = False
+		print("Loop number:",i,"/",len(input_list)-1)
 		for idx in range(len(input_list) - i - 1):
 			if input_list[idx] > input_list[idx + 1]:
 				input_list[idx], input_list[idx + 1] = input_list[idx + 1], input_list[idx]
+				swapped = True
+		if not swapped:
+			break
 	return input_list
 
 print(bubble_sort(test_list))
