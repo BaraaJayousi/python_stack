c0cdc046f1fa2ce399b2d2c8e03ab3bc5998eb24
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-08 15:33:25 +0200

    added make_deposit method and called it through michael instance to increase the account balance for this instance. Also deepened my understanding of self as it represents the attriputes and methods of the Class (User) passed to the method and it is not passed upon calling the method as it is called implicitly

--- _python/OOP/user.py
+++ _python/OOP/user.py
@@ -5,8 +5,17 @@ class User:
         self.email = email
         self.account_balance = 0
 
+    def make_depoist(self, amount):
+        self.account_balance += amount
+
 #create new instances of the class User
 michael = User("Michael", "mich@example.com")
 anna = User("Anna", "anna@example.com")
 
-print(michael.name)
+print(f"Username: {michael.name} Balance: {michael.account_balance}$")
+michael.make_depoist(100)
+print("Deposited 100$")
+print(f"Username: {michael.name} Balance: {michael.account_balance}$")
+michael.make_depoist(200)
+print("Deposited 200$")
+print(f"Username: {michael.name} Balance: {michael.account_balance}$")
