3034c549d62cc9fb7c3e67368b8a627560bbe8ec
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-08 16:08:45 +0200

    Implemented chaining methods on User class

--- _python/OOP/user.py
+++ _python/OOP/user.py
@@ -9,22 +9,23 @@ class User:
     def make_depoist(self, amount):
         self.account_balance += amount
         print(f"{self.name} Deposited {amount}$\n")
+        return self
     
     #Withdraws money from the account balance
     def make_withdrawal(self, amount):
         self.account_balance -= amount
         print(f"{self.name} Withdrawn {amount}$\n")
-
+        return self
     #display user's name and current balance
     def display_user_balance(self):
         print(f"Username: {self.name} Balance: {self.account_balance}$\n")
-
+        return self
     #Trnsfer amount of money from current user to a specified user of type User
     def make_transfer(self, other_user, amount):
         self.make_withdrawal(amount)
         other_user.make_depoist(amount)
         print(f"Money transfer of {amount}$. From {self.name} To {other_user.name}\n")
-
+        return self
 
 #create new instances of the class User
 michael = User("Michael", "mich@example.com")
@@ -32,28 +33,15 @@ anna = User("Anna", "anna@example.com")
 monty = User("Monty","monty@example.com")
 
 #First user
-michael.make_depoist(100)
-michael.make_depoist(200)
-michael.make_depoist(212)
-michael.make_withdrawal(250)
+michael.make_depoist(100).make_depoist(200).make_depoist(212).make_withdrawal(250)
 
 #Second User
-anna.make_depoist(300)
-anna.make_depoist(2100)
-anna.make_withdrawal(32)
-anna.make_withdrawal(1000)
-anna.display_user_balance()
+anna.make_depoist(300).make_depoist(2100).make_withdrawal(32).make_withdrawal(1000).display_user_balance()
 
 
 
 #Thrid User
-monty.make_depoist(20)
-monty.make_withdrawal(15)
-monty.make_withdrawal(2.5)
-monty.make_withdrawal(2)
-monty.display_user_balance()
+monty.make_depoist(20).make_withdrawal(15).make_withdrawal(2.5).make_withdrawal(2).display_user_balance()
 
 #Trnasfer between first and third users
-michael.make_transfer(monty,100)
-michael.display_user_balance()
-monty.display_user_balance()
+michael.make_transfer(monty,100).display_user_balance().display_user_balance()
