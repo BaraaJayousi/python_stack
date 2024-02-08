07f96b518a05a1f9ab00cb12987b8adf2411125d
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-08 14:19:11 +0200

    added User class contrustor (__ini__) method and set initial attributes, changed name attribute for michael instace

--- _python/OOP/user.py
+++ _python/OOP/user.py
@@ -1,7 +1,16 @@
 #Create a new class class User
-Class User:
-    pass
+class User:
+    def __init__(self):
+        self.name = "Baraa"
+        self.email = "example@example.com"
+        self.account_balance = 0
 
 #create new instances of the class User
 michael = User()
 anna = User()
+
+print(michael.name)
+
+#setting michael name
+michael.name = "Michael"
+print(michael.name)
