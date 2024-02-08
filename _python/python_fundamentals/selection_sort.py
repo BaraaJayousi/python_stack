905caf18c815d79d5013a8e8cc958a186d6589fe
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-08 03:34:08 +0200

    implemented selection sort algorithm

---
+++ _python/python_fundamentals/selection_sort.py
@@ -0,0 +1,11 @@
+test_list = [3,1,0,4,2,5,6,7,8]*8000
+def selection_sort(input_list):
+    for i in range (len(input_list)):
+        min_idx = i
+        for idx in range(i,len(input_list)):
+            if input_list[min_idx] > input_list[idx]:
+                min_idx = idx
+        input_list[i], input_list[min_idx] = input_list[min_idx], input_list[i]
+    return input_list
+
+print(selection_sort(test_list))
