0e4494694e61d4d4995901ef0138b4d5a5f6246e
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-12 20:24:49 +0200

    finished checkboard assignment with ninja bonus

---
+++ flask/flask_fundamentals/checkboard/checkboard.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def checkboard_constant():
+    return render_template("index.html", x=8, y=8)
+
+@app.route("/<int:x>/")
+def checkboard_x_variable(x):
+    return render_template("index.html",x = int(x), y = 8)
+
+@app.route("/<int:x>/<int:y>/")
+def checkboard_x_y_variable(x,y):
+    return render_template("index.html", x =int(x), y=int(y))
+
+@app.route("/<int:x>/<int:y>/<col1>/<col2>/")
+def checkboard_x_y_color_variable(x,y,col1,col2):
+    return render_template("index.html",x =int(x), y=int(y), col1=col1, col2=col2)
+
+if __name__ == "__main__":
+    app.run(debug=True)
