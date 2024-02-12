eebd94cfe49f1114c7606e8ccbf0be2eb4971aea
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-12 03:02:38 +0200

    Started with flask micro framework

---
+++ flask/flask_fundamentals/hello_flask/hello.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(e):
+    return f"the page is not found, call me to give you hints"
+
+@app.route('/')
+def hello_world():
+    return "<h1>Hello World</h1>"
+
+if __name__  == '__main__':
+    app.run(debug=True)
