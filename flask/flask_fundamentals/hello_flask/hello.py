d1bee4ca878c44f23315c2c72be95d6f70d9d1fc
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-13 07:04:13 +0200

    modified hello_flask project by adding lists.html template and /lists/ route to render data from an array of integers and dictionaries

--- flask/flask_fundamentals/hello_flask/hello.py
+++ flask/flask_fundamentals/hello_flask/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.errorhandler(404)
@@ -9,9 +9,23 @@ def not_found(e):
 def hello_world():
     return "<h1>Hello World</h1>"
 
-@app.route('/hello/<name>')
+@app.route('/hello/<name>/')
 def hello(name):
     return f"hello, {name}"
+
+
+@app.route('/lists/')
+def render_lists():
+    #hard coded data, instead of retriving from a database
+    student_info = [
+        {'name': 'Michael', 'age': 35},
+        {'name': 'John', 'age': 22},
+        {'name': 'Mark', 'age': 25},
+        {'name': 'KB', 'age': 27},
+    ]
+
+    return render_template("lists.html", random_numbers=[3,1,5], students = student_info)
+
 
 if __name__  == '__main__':
     app.run(debug=True)
