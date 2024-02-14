e3a36ce608df50f62d1b427f20e1758907beabf7
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-14 06:45:34 +0200

    added radios and checkboxes to the form

--- flask/flask_fundamentals/dojo_survey/main.py
+++ flask/flask_fundamentals/dojo_survey/main.py
@@ -11,6 +11,8 @@ def render_form():
 def render_results():
     print("Got post info")
     print(request.form)
+    print(request.form.getlist('email_agreement'))
     return render_template("result.html",result=request.form)
+
 if __name__ == "__main__":
     app.run(debug=True)
