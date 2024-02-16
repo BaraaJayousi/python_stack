d058d74ac7faa31bdbb5fa52350426f8d0151b79
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-17 01:57:42 +0200

    added redirect function

--- flask/flask_fundamentals/form_test/server.py
+++ flask/flask_fundamentals/form_test/server.py
@@ -6,13 +6,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/users',methods=['POST'])
+@app.route('/users/',methods=['POST'])
 def create_user():
     print("Got Post Info")
     print(request.form)
     name_from_form = request.form['name']
     email_from_form = request.form['email']
-    return render_template("show.html", name_on_template = name_from_form, email_on_template = email_from_form)
+    # return render_template("show.html", name_on_template = name_from_form, email_on_template = email_from_form)
+    return redirect('/show/')
+
+@app.route("/show/")
+def show_user():
+    print("showing the user info from the form")
+    print(request.form)
+    return render_template("show.html")
 
 
 if __name__ == "__main__":
