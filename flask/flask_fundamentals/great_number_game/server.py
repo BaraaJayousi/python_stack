f758784fcab9ac197358127ae9002f07da73b8e3
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-17 12:40:08 +0200

    added trail count

--- flask/flask_fundamentals/great_number_game/server.py
+++ flask/flask_fundamentals/great_number_game/server.py
@@ -4,19 +4,25 @@ import random
 app = Flask(__name__)
 
 app.secret_key = '123456789'
-random_number = random.randint(1,100)
+# random_number = random.randint(1,100)
 
 @app.route('/')
 def start_game():
-    session['random_num'] = random_number
-    print(session['random_num'])
-    if "answer" in session:
-        print(session['answer'])
+    # session['random_num'] = random_number
+    if "random_num" not in session:
+        return redirect('/reset/')
+    # if "answer" in session:
+    #     print(session['answer'])
     return render_template("index.html")
 
 @app.route("/check_guess/", methods=["POST"])
 def check_guess():
     guess= int(request.form['guess'])
+    if 'guess_count' not in session:
+        session['guess_count'] = 1
+    else:
+        session['guess_count'] += 1
+
     if guess > session['random_num']:
         session['answer'] = 'high'
     elif guess < session['random_num']:
@@ -25,9 +31,11 @@ def check_guess():
         session['answer'] = 'correct'
     return redirect('/')
 
-@app.route("/reset/", methods=["POST"])
+@app.route("/reset/", methods=["GET"])
 def reset_game():
     session.clear()
+    random_number = random.randint(1,100)
+    session['random_num'] = random_number
     return redirect("/")
 if __name__ == "__main__":
     app.run(debug=True)
