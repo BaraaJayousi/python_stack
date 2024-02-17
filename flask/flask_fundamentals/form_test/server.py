a5a5180f6dd0280887459ac55cc0da52b44656c8
Baraa Jayousi <b.jayousi98@gmail.com>
2024-02-17 02:12:43 +0200

    added and implemented sessions with redirect

--- flask/flask_fundamentals/form_test/server.py
+++ flask/flask_fundamentals/form_test/server.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template, request, redirect
+from flask import Flask,render_template, request, redirect, session
 
 app = Flask(__name__)
+
+app.secret_key = "123456789"
 
 @app.route('/')
 def index():
@@ -10,8 +12,8 @@ def index():
 def create_user():
     print("Got Post Info")
     print(request.form)
-    name_from_form = request.form['name']
-    email_from_form = request.form['email']
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
     # return render_template("show.html", name_on_template = name_from_form, email_on_template = email_from_form)
     return redirect('/show/')
 
