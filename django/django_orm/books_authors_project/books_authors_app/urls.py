91d078abcfd129913d5fdabfef1da09fff0f52a9
Baraa Jayousi <b.jayousi98@gmail.com>
2024-03-10 23:18:59 +0200

    finished books bit adding books template with table listing all avaliable book specific book template with authors addition to the specified book

--- django/django_orm/books_authors_project/books_authors_app/urls.py
+++ django/django_orm/books_authors_project/books_authors_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-
+from .  import views
 urlpatterns = [
-    
+    path('',views.books),
+    path('add-book', views.add_books),
+    path('books/<int:book_id>', views.show_book),
+    path('books/<int:book_id>/add-book-author', views.add_book_author),
 ]
--- django/django_orm/books_authors_project/books_authors_app/views.py
+++ django/django_orm/books_authors_project/books_authors_app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
+
+def books(request):
+    context={
+        "books": Books.objects.all()
+    }
+    return render(request,'books.html', context)
+
+def add_books(request):
+    if request.method == 'POST':
+        Books.objects.create(title=request.POST['book_title'], desc=request.POST['book_desc'])
+    return redirect('/')
+
+def show_book(request, book_id):
+    context={
+        "book": Books.objects.get(id=book_id),
+        "authors": Authors.objects.exclude(books=Books.objects.get(id=book_id))
+    }
+    return render(request, 'book_details.html', context)
+
+def add_book_author(request, book_id):
+    if request.method == 'POST':
+        Books.objects.get(id=book_id).authors.add(Authors.objects.get(id=request.POST['book_author']))
+        return redirect(f"/books/{book_id}")
+    return redirect("/")
+
