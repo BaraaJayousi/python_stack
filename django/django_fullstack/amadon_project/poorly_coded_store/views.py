7df9ef4fbeb044a10302b9b2080ffaef1475b381
Baraa Jayousi <b.jayousi98@gmail.com>
2024-03-26 02:14:04 +0200

    made checkout modifications to render data from the models

--- django/django_fullstack/amadon_project/poorly_coded_store/views.py
+++ django/django_fullstack/amadon_project/poorly_coded_store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Order, Product
+from django.db.models import Sum
 
 def index(request):
     context = {
@@ -8,9 +9,20 @@ def index(request):
     return render(request, "store/index.html", context)
 
 def checkout(request):
-    quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
-    print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    
+    context = {
+        "invoice_total": Order.objects.all().order_by("-created_at").first().total_price,
+        "all_orders_items_total": Order.objects.aggregate(Sum("quantity_ordered"))['quantity_ordered__sum'],
+        "total_spend": Order.objects.aggregate(Sum("total_price"))['total_price__sum']
+    }
+    return render(request, "store/checkout.html", context)
+    
+
+def process_order(request):
+    if request.method == 'POST':
+        quantity_from_form = int(request.POST["quantity"])
+        price_from_form = float(request.POST["price"])
+        total_charge = quantity_from_form * price_from_form
+        print("Charging credit card...")
+        Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+        return redirect('/checkout')
